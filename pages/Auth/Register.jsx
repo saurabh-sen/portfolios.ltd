1ac94a32baa741ca16d67c075c33ced270558aae
Saurabh Sen <sensaurabh1212@gmail.com>
2022-09-01 22:46:13 +0530

    Update Register.jsx

--- pages/Auth/Register.jsx
+++ pages/Auth/Register.jsx
@@ -1,4 +1,4 @@
-import { React, useState } from 'react'
+import { React, useState, useEffect } from 'react'
 import styles from './Register.module.css'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
